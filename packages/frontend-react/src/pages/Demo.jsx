505923e1edd28ad60a433e979b72d074f998795e
pnkll <kavasakya@gmail.com>
2022-08-17 12:40:31 +0300

    yup validator

--- packages/frontend-react/src/pages/Demo.jsx
+++ packages/frontend-react/src/pages/Demo.jsx
@@ -9,32 +9,17 @@ import Table from "../components/Table/Table.jsx"
 import * as axios from "axios"
 import './Demo.scss'
 import Select from "../components/Select/Select.jsx";
+import * as yup from 'yup'
+import { emailValidator, passwordValidator, phoneValidator, stringValidator } from "../utils/yupValidation.js";
 
 export default React.memo(function Form() {
 
-    const validate = values => {
-        const errors = {};
-
-        if (!values.password) {
-            errors.password = 'Please fill in your password';
-        } else if (values.password.length < 8) {
-            errors.password = 'Must be 8 characters or more';
-        }
-
-        if (!values.email || values.email === 'Email') {
-            errors.email = 'Please fill in your email';
-        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-            errors.email = 'Invalid email address';
-        }
-
-        if (!values.phone) {
-            errors.phone = 'Please fill phone'
-        } else if (values.phone.length < 14 || values.phone.split('').find(el => el === '_')) {
-            errors.phone = 'Pleease fill this'
-        }
-
-        return errors;
-    };
+    const validationSchema = yup.object({
+        email: emailValidator(true),
+        password: passwordValidator(true),
+        phone: phoneValidator(),
+        info: stringValidator(true, 'Info')
+    })
 
     const formik = useFormik({
         initialValues: {
@@ -43,7 +28,7 @@ export default React.memo(function Form() {
             rememberMe: false,
             phone: '',
         },
-        validate,
+        validationSchema,
         onSubmit: values => {
             console.log(values)
         }
@@ -54,6 +39,7 @@ export default React.memo(function Form() {
     const [currentPage, setCurrentPage] = useState(1)
     const [itemsCount, setItemsCount] = useState(5)
     const [totalItemsCount, setTotalItemsCount] = useState(1000)
+
     const getPosts = async () => {
         setDisabled(true)
         const response = await axios.get(`https://jsonplaceholder.typicode.com/posts?_page=${currentPage}&_limit=${itemsCount}`)
@@ -74,15 +60,15 @@ export default React.memo(function Form() {
     return (
         <>
             <div className="demo-page__container">
-                    <form className="demo-page__form" onSubmit={(e) => { e.preventDefault(); formik.handleSubmit(e) }} style={{ width: "500px" }}>
-                        <div className="demo-page__form__elem"><Input formik={formik} id={'email'} name={'email'} type={'text'} placeholder={'Email'} label='email' required={true}/></div>
-                        <div className="demo-page__form__elem"><Input formik={formik} id='password' name='password' type='password' placeholder='Password' label='password' required={true}/></div>
-                        <div className="demo-page__form__elem"><PhoneInput formik={formik} id='phone' name='phone' defaultCode={7} label='Phone' required={true}/></div>
-                        <div className="demo-page__form__elem"><DatePicker formik={formik} id='date' name='date' placeholder='Выберите дату' showTimeSelect={true} timeIntervals={30} /></div>
-                        <div className="demo-page__form__elem"><TextArea formik={formik} id='info' name='info' placeholder=' ' isRequired={true} rows={5} label='Расскажите о себе'/></div>
-                        <div className="demo-page__form__elem"><Select className='demo-page__form__elem__select' options={[{label: 'jkfhdsakj', value:''},{label: 'jkfhdsakj', value:''}]}/></div>
-                        <div className="demo-page__form__elem"> <Button text='Sign in' type='submit' disabled={true}/></div>
-                    </form>
+                <form className="demo-page__form" onSubmit={(e) => { e.preventDefault(); formik.handleSubmit(e) }} style={{ width: "500px" }}>
+                    <div className="demo-page__form__elem"><Input formik={formik} id={'email'} name={'email'} type={'text'} placeholder={'Email'} label='email' /></div>
+                    <div className="demo-page__form__elem"><Input formik={formik} id='password' name='password' type='password' placeholder='Password' label='password' /></div>
+                    <div className="demo-page__form__elem"><PhoneInput formik={formik} id='phone' name='phone' defaultCode={7} label='Phone' /></div>
+                    <div className="demo-page__form__elem"><DatePicker formik={formik} id='date' name='date' placeholder='Выберите дату' showTimeSelect={true} timeIntervals={30} /></div>
+                    <div className="demo-page__form__elem"><TextArea formik={formik} id='info' name='info' placeholder=' ' isRequired={true} rows={5} label='Расскажите о себе' /></div>
+                    <div className="demo-page__form__elem"><Select className='demo-page__form__elem__select' options={[{ label: 'jkfhdsakj', value: '' }, { label: 'jkfhdsakj', value: '' }]} /></div>
+                    <div className="demo-page__form__elem"> <Button text='Sign in' type='submit' /></div>
+                </form>
                 <div className="demo-page__table">
                     {!data ? <Button text='fetch users' disabled={disabled} onClick={() => getPosts()} />
                         : <Table data={data} columns={columns} currentPage={currentPage} setCurrentPage={setCurrentPage} itemsCount={itemsCount} totalItemsCount={totalItemsCount} />}

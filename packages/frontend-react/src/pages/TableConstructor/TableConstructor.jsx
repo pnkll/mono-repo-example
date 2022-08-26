b2c6c497bcf24b841afef58a6120ffc10125888d
pnkll <kavasakya@gmail.com>
2022-08-26 18:59:57 +0300

    pre rebuild constructor

--- packages/frontend-react/src/pages/TableConstructor/TableConstructor.jsx
+++ packages/frontend-react/src/pages/TableConstructor/TableConstructor.jsx
@@ -2,90 +2,96 @@ import React, { useRef, useState } from "react";
 import HeaderLayout from '../../page_layouts/HeaderLayout/HeaderLayout.jsx'
 import './TableConstructor.scss'
 import ReactSelect from "react-select";
-import { ArrowDownIcon, ArrowUpIcon } from "@heroicons/react/solid";
+import { ArrowDownIcon, ArrowUpIcon, XIcon } from "@heroicons/react/solid";
 import { isEmpty, isNil } from "lodash";
 import useDrag from "../../hooks/useDrag.js";
 import Button from "../../components/Button/Button.jsx";
 import TableConstructorForm from "../../components/TableConstructorForm/TableConstructorForm.jsx";
 import Table from "../../components/Table/Table.jsx"
-import { useTable } from "react-table";
+import Input from "../../components/Input/Input.jsx";
+import * as Yup from 'yup'
 
 export default React.memo(function TableConstructor() {
-    // const [headers, setHeaders] = useState([
-    //     { order: 1, title: '', code: '', type: 'string', required: false, unique: false, toIndex: false, visible: false, focused: true },
-    // ])
-    // const handleAppend = () => {
-    //     setHeaders([...headers, { order: headers[headers.length - 1].order + 1, title: '', code: '', type: 'string', required: false, unique: false, toIndex: false, visible: false, focused: true }])
-    // }
-    // const handleRemove = (idx) => {
-    //     setHeaders(headers.filter((el, index) => index !== idx))
-    // }
-    // const handleChange = (index, field, value) => {
-    //     setHeaders(headers.map((el, idx) => idx === index ? { ...el, [field]: value } : el))
-    // }
-    // const options = [
-    //     { label: 'string', value: 'string' },
-    //     { label: 'number', value: 'number' },
-    //     { label: 'date', value: 'date' },
-    // ]
-    // const {sortItems,dragStartHandler,dragEndHandler,dragOverHandler,dropHandler}=useDrag(setHeaders,headers)
-    const [data, setData] = useState([])
-    const [forms, setForms] = useState([
-        { order: 1 }
+    const [headers, setHeaders] = useState([
+        { order: 1, title: '', code: '', type: 'string', required: false, unique: false, toIndex: false, visible: false, focused: true },
     ])
-    const appendHandler = () => {
-        setForms([...forms, { order: forms.length>0?forms[forms.length - 1].order + 1:1 }])
+    const handleAppend = () => {
+        setHeaders([...headers, { order: headers[headers.length - 1].order + 1, title: '', code: '', type: 'string', required: false, unique: false, toIndex: false, visible: false, focused: true }])
     }
-    const removeHandler = (idx) => {
-        setForms(forms.filter((form, index) => form.order !== idx))
-        setData(data.filter((data, index) => data.id !== idx))
+    const handleRemove = (idx) => {
+        headers.length>1&&setHeaders(headers.filter((el, index) => index !== idx))
     }
-    const { sortItems, dragStartHandler, dragEndHandler, dragOverHandler, dropHandler } = useDrag(setForms, forms)
-    const submitHandler = () => {
-        setData(data.map(el => el && { ...el, isSubmit: true }))
+    const handleChange = (index, field, value) => {
+        setHeaders(headers.map((el, idx) => idx === index ? { ...el, [field]: value } : el))
     }
-    const columnsRef = useRef([])
-    // const columns = [
-    //     {Header: 'tifds', accessor: 'fdsjh', 
-    //     Cell: ({ cell: { value } }) => value || "-",
-    //     props: { type: 'string', required: true, unique: false, toIndex: false}},
-    //     {Header: 'tifdsfds', accessor: 'fdsdfsjh', props: { type: 'string', required: true, unique: true, toIndex: false}},
-    //     {Header: 'tissadfds', accessor: 'fddsjh', props: { type: 'number', required: false, unique: false, toIndex: false}},
-    //     {Header: 'tifdsdads', accessor: 'fdsdfdsfgjh', props: { type: 'string', required: true, unique: false, toIndex: false}},
-    //     {Header: 'tiasfds', accessor: 'fffdsjh', props: { type: 'string', required: true, unique: false, toIndex: false}},
-    // ]
-    const dataa = [{
-        id: 1,
-        firstname: 'Jeka',
-        lastname: 'Ormano',
-        age: null
-    }]
-    // const { headerGroups } = useTable({columns, dataa})
-    // console.log(headerGroups)
+
+
+    const schema = Yup.object().shape({
+        title: Yup.string().min(3,'min 3').required('Обязательное поле'),
+        code: Yup.string().min(5, 'min 5').matches(/^[a-zA-Z]+$/,'только английские буквы'),
+    });
+    const [errors,setErrors]=useState([])
+    const options = [
+        { label: 'string', value: 'string' },
+        { label: 'number', value: 'number' },
+        { label: 'date', value: 'date' },
+    ]
+    const [columns, setColumns] = useState([])
+    const { sortItems, dragStartHandler, dragEndHandler, dragOverHandler, dropHandler } = useDrag(setHeaders, headers)
+    const submitHandler = async () => {
+        setErrors([])
+        let fakeColumns = []
+        let fakeErrors = []
+            headers.map(header=>schema.validate(header, { abortEarly: false })
+            .then(()=>{
+                fakeColumns = [...fakeColumns, {Header: header.title, accessor: header.code}]
+                setColumns(fakeColumns)
+            })
+            .catch((err) => {
+                console.log(err.inner)
+                const errorsObj = err.inner.reduce((acc, error) => {
+                  return {
+                    ...acc,
+                    [error.path]: {type: error.type, error: error.errors},
+                  }
+                }, {})
+                fakeErrors=[...fakeErrors, {order: header.order, errors: errorsObj}]
+                setErrors(fakeErrors)
+            }))
+        }
     return (
         <>
             <HeaderLayout>
                 <div style={{ display: 'flex', gap: '20px', flexDirection: 'column' }}>
                     <div className="" style={{ display: 'flex', gap: '10px' }}>
-                        <Button text='Добавить поле' color='green' handleClick={appendHandler} />
+                        <Button text='Добавить поле' color='green' handleClick={handleAppend} />
                         <Button text='Создать таблицу' handleClick={submitHandler} />
                     </div>
                     <div style={{ display: 'flex', overflowX: 'scroll', overflowY: 'visible', }} className={'table-constructor__wrapper'}>
-                        {/* {headers && headers.sort(sortItems).map((header, index) =>
+                        {headers && headers.sort(sortItems).map((header, index) => 
                             <ul
                                 draggable={true}
-                                onDragStart={(e) => dragStartHandler(e,header)}
+                                onDragStart={(e) => dragStartHandler(e, header)}
                                 onDragEnd={(e) => dragEndHandler(e)}
                                 onDragLeave={(e) => dragEndHandler(e)}
                                 onDragOver={(e) => dragOverHandler(e)}
-                                onDrop={(e) => dropHandler(e,header)}
-                                className={`table-constructor__elem ${headers.length<2?'single':''}`}
+                                onDrop={(e) => dropHandler(e, header)}
+                                className={`table-constructor__elem ${headers.length < 2 ? 'single' : ''}`}
                                 key={index} style={{ listStyle: 'none', padding: 10, margin: 0 }}>
                                 <li style={{ display: 'flex', gap: 10 }}><label>Title:</label><input type='text' value={header.title} onChange={(e) => handleChange(index, 'title', e.target.value)} /></li>
+                                {errors.find(error=>error.order===header.order&&error.errors?.title?.type!=='required')&&<li>
+                                            {errors.find(error=>error.order===header.order&&error.errors?.title.type!=='required').errors.title.error[0]}
+                                            </li>}
+                                            {errors.find(error=>error.order===header.order&&error.errors?.title?.type==='required')&&<li>
+                                            *
+                                            </li>}
                                 <li >
                                     <p onClick={() => setHeaders(headers.map((el, idx) => idx === index ? { ...el, visible: !el.visible } : el))} style={{ display: 'flex', gap: 10 }}>{header.visible ? <><ArrowUpIcon width={10} />Скрыть</> : <><ArrowDownIcon width={10} />Показать</>}&nbsp;свойства</p>
                                     <ul className={`props ${header.visible ? '' : 'closed'}`} style={{ listStyle: 'none', padding: 0, margin: 0, }}>
-                                        <li style={{ display: 'flex', gap: 10 }}><label>Code:</label><input type='text' value={header.code} onChange={(e) => handleChange(index, 'code', e.target.value)} /></li>
+                                        <li style={{ display: 'flex', gap: 10, }}><label>Code:</label><input type='text' value={header.code} onChange={(e) => handleChange(index, 'code', e.target.value)} /></li>
+                                        {errors.find(error=>error.order===header.order&&error.errors?.code?.type!=='required')&&<li>
+                                            {errors.find(error=>error.order===header.order&&error.errors?.code).errors.code.error[0]}
+                                            </li>}
                                         <li><ReactSelect options={options} value={{ label: header.type, value: header.type }} onChange={(e) => handleChange(index, 'type', e.value)}></ReactSelect></li>
                                         <li>req:<input type='checkbox' checked={header.required} onChange={(e) => handleChange(index, 'required', e.target.checked)} /></li>
                                         <li>unique:<input type='checkbox' checked={header.unique} onChange={(e) => handleChange(index, 'unique', e.target.checked)} /></li>
@@ -93,36 +99,12 @@ export default React.memo(function TableConstructor() {
                                     </ul>
                                 </li>
                                 <li style={{ display: 'flex', justifyContent: 'center' }}>
-                                    <Button color='red' handleClick={()=>handleRemove(index)} text='Удалить поле'/>
+                                    <Button color='red' handleClick={() => handleRemove(index)} text='Удалить поле' />
                                 </li>
                             </ul>
-                        )} */}
-                        {forms.sort(sortItems).map((form, index) => <div key={index} style={{ display: 'flex', flexDirection: 'column' }}>
-                            <div className="drag__elem"
-                                draggable={true}
-                                onDragStart={(e) => dragStartHandler(e, form)}
-                                onDragEnd={(e) => dragEndHandler(e)}
-                                onDragLeave={(e) => dragEndHandler(e)}
-                                onDragOver={(e) => dragOverHandler(e)}
-                                onDrop={(e) => dropHandler(e, form)}>
-                                <TableConstructorForm
-                                    columns={columnsRef.current}
-                                    removeHandler={removeHandler}
-                                    index={index}
-                                    order={form.order}
-                                    key={index}
-                                    setData={setData}
-                                    data={data} />
-                                <Button text='delete' handleClick={() => removeHandler(index)} />
-                            </div>
-                        </div>)}
+                        )}
                     </div>
-                    <div className="">
-                        {!isEmpty(columnsRef.current)&&<Table 
-                        columns={columnsRef.current}
-                        data={dataa}
-                        />}
-                    </div>
+                    <Table columns={columns} data={[]} />
                 </div>
             </HeaderLayout>
         </>

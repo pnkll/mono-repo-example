ff4480ff77e4cf4fdf61e58dc0bcf09fbc42ca25
pnkll <kavasakya@gmail.com>
2022-09-03 16:02:02 +0300

    profile update

--- packages/frontend-react/src/forms/ProfileForm.jsx
+++ packages/frontend-react/src/forms/ProfileForm.jsx
@@ -1,20 +1,25 @@
-import { useFormik } from 'formik';
+import {useFormik} from 'formik';
 import React from 'react';
+import Input from "../components/Input/Input";
 
-export default React.memo(function ProfileForm(){
+export default React.memo(function ProfileForm() {
     const formik = useFormik({
-        initialValues:{
-
-        },
-        onSubmit:{
-
-        }
+        initialValues: {},
+        onSubmit: {}
     })
-   return(
-       <>
-       <form onSubmit={(e)=>{e.preventDefault();formik.handleSubmit()}}>
-
-       </form>
-       </>
-   )
+    return (
+        <>
+            <form onSubmit={(e) => {
+                e.preventDefault();
+                formik.handleSubmit()
+            }}>
+                <Input formik={formik} label={'Username'} id={'username'} name={'username'}/>
+                <Input formik={formik} label={'First name'} id={'firstname'} name={'firstname'}/>
+                <Input formik={formik} label={'Last name'} id={'lastname'} name={'lastname'}/>
+                <Input formik={formik} label={'Organization'} id={'organization'} name={'organization'}/>
+                <Input formik={formik} label={'Phone'} id={'phone'} name={'phone'}/>
+                <
+            </form>
+        </>
+    )
 })

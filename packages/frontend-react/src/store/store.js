918d752b40320e7bd4d065b68e3cf27461bfc473
pnkll <kavasakya@gmail.com>
2022-10-02 21:30:12 +0300

    middleware for persist store

--- packages/frontend-react/src/store/store.js
+++ packages/frontend-react/src/store/store.js
@@ -1,4 +1,4 @@
-import {combineReducers, configureStore} from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import appReducer from './slices/appSlice'
 import userReducer from './slices/userSlice'
 import sidebarReducer from './slices/sidebarSlice'
@@ -6,7 +6,16 @@ import callControlReducer from './slices/callContollSlice'
 import rolesReducer from './slices/rolesSlice'
 import notificationsReducer from './slices/notificationsSlice'
 import { Api } from '../services/api'
-import { persistStore, persistReducer } from 'redux-persist'
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 
 const persistConfig = {
@@ -25,12 +34,16 @@ const rootReducer = combineReducers({
   [Api.reducerPath]: Api.reducer
 })
 
-const persistedReducer = persistReducer(persistConfig,rootReducer)
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(Api.middleware),
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(Api.middleware),
 })
 
 export const persistor = persistStore(store)

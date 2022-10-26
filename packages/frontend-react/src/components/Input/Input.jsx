4fa76ce7fc980696b7eb74d1c29b57407355c362
pnkll <kavasakya@gmail.com>
2022-10-26 10:16:07 +0300

    input styles fix

--- packages/frontend-react/src/components/Input/Input.jsx
+++ packages/frontend-react/src/components/Input/Input.jsx
@@ -1,44 +1,43 @@
-import { BeakerIcon } from "@heroicons/react/solid"
-import { concat, isNil } from "lodash"
+import { isNil } from "lodash"
 import React from "react"
-import './Input.scss'
+import s from './Input.module.scss'
+import classNames from 'classnames/bind'
 
-export default function Input({formik,label,placeholder,id,name,type,autoComplete,className,required,handleChange,value,readonly=false,defaultStyles=true,setFocus=null }){
-    const classNamePreffix = className || 'input-field'
-    const changeHandler = (e) =>{
-                //!isNil(formik)?formik.setFieldValue(id,e.target.value):handleChange(e)
-                !isNil(formik)?formik.handleChange(e):handleChange(e)
+const cx = classNames.bind(s)
+
+export default function Input({ formik, label, placeholder, id, name, type, autoComplete, className, required, handleChange, value, readonly = false, defaultStyles = true, setFocus = null, ...other }) {
+    const cls = className || 'input-field'
+    const changeHandler = (e) => {
+        !isNil(formik) ? formik.handleChange(e) : handleChange(e)
     }
-    const notEditStyles = {
-        border: 0,
-    fontSize: '13px',
-    borderBottom: '1px solid',
-    borderRadius: '4px 4px 0 0',
-    paddingLeft: 0,
-    paddingRight: '12px',
-    }
-    return(
+    return (
         <>
-        <div className={`${classNamePreffix}__container`}>
-            {!isNil(label)&&<label className={`${classNamePreffix}__label`}>{label} {required && '*'}</label>}
-            <input
-                required={required}
-                id={id}
-                name={name}
-                value={!isNil(formik)?formik.values[id]:value}
-                placeholder={placeholder}
-                onChange={changeHandler}
-                type={type}
-                onClick={()=>!isNil(formik)&&formik.setFieldError(id,'')}
-                autoComplete={autoComplete || 'off'}
-                className={`${classNamePreffix}__input ${!isNil(formik)&&formik.touched[name]&&formik.errors[name]?'error':''}`}
-                readOnly={readonly}
-                style={readonly?defaultStyles?notEditStyles:{}:{}}
-                onFocus={setFocus?()=>setFocus(id):null}
-                onBlur={setFocus?()=>setFocus(null):null}
-            />
-            {!isNil(formik)&&formik.touched[name]&&formik.errors[name]&&<div className={`${classNamePreffix}__error`}>{formik.errors[name]}</div>}
-        </div>
+            <div className={s[`${cls}__container`]}>
+                {!isNil(label) && <label className={s[`${cls}__label`]}>
+                    {label} {required && '*'}
+                </label>}
+                <input
+                    required={required}
+                    id={id}
+                    name={name}
+                    value={!isNil(formik) ? formik.values[id] : value}
+                    placeholder={placeholder}
+                    onChange={changeHandler}
+                    type={type}
+                    onClick={() => !isNil(formik) && formik.setFieldError(id, '')}
+                    autoComplete={autoComplete || 'off'}
+                    className={cx({ [`${cls}__input`]: true, error: !isNil(formik) && formik.touched[name] && formik.errors[name], readonly: readonly })}
+                    readOnly={readonly}
+                    onFocus={setFocus ? () => setFocus(id) : null}
+                    onBlur={setFocus ? () => setFocus(null) : null}
+                    {...other}
+                />
+
+                {!isNil(formik) && formik.touched[name] && formik.errors[name]
+                    && <div className={s[`${cls}__error`]}>
+                        {formik.errors[name]}
+                    </div>}
+            </div>
         </>
     )
 }

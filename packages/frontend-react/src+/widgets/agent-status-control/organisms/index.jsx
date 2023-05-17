8b11edabfadcf7fcf3264b90bbba928e27c289d8
pnkll <kavasakya@gmail.com>
2023-05-17 10:58:46 +0300

    wip

---
+++ packages/frontend-react/src+/widgets/agent-status-control/organisms/index.jsx
@@ -0,0 +1,11 @@
+import s from './styles.module.scss'
+
+import { ReactTable } from 'shared/ui/react-table-7'
+
+export function AgentStatusControl(){
+   return(
+      <>
+         <ReactTable.view model={}/>
+      </>
+  )
+}
